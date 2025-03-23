a450f09c34343d0e10eb25fb364344287443aed5
arash <homayounfar.arash@gmail.com>
2025-03-23 17:19:30 +0330

    update

--- models/models.py
+++ models/models.py
@@ -15,7 +15,8 @@ class SdHseFormsProjects(models.Model):
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
-    project_code = fields.Char()
+    project_name = fields.Many2one('sd_projects.projects')
+    project_code = fields.Char(related='project_name.project_code')
     base_address = fields.Char(compute="_base_address")
     link_address = fields.Char(compute="_base_address")
     qr_code = fields.Binary("QR Code", compute='generate_qr_code')
